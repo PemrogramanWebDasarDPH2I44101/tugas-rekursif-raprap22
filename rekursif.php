e0c7e1cf6d45e65d7312306a4b8b75f60483a114
Ridho <42725295+raprap22@users.noreply.github.com>
2018-09-10 21:43:12 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,38 @@
-<?php
+<?php 
+function bintang($w, $x){
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+	if ($w > 0) {
+		if ($x > 0) {
+			echo "* ";
+			$x--;
+			bintang($w, $x);
+		}
+		else{
+			echo "<br>";
+			$w--;
+			bintang($w, $w);
+		}
+	}
 }
-rekursif(5);
-?>
+bintang (5, 5);
+
+echo "--------------------------------------- <br><br>";
+
+
+function angka($w, $x){
+
+	if ($w <= 5) {
+		if ($x <= $w) {
+			echo "$x ";
+			$x++;
+			angka($w, $x, 5);
+		}
+		else{
+			echo "<br>";
+			$w++;
+			angka($w, 1, 5);
+		}
+	}
+}
+angka (1, 1, 5);
+ ?>
